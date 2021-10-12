f42646fd0a0085b7d40073299cb14aeb0a60f84c
Rafał Kostecki <edward-12@o2.pl>
2021-10-13 00:29:46 +0200

    AG-32 Extend user config character section

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,4 @@
 
 - AG-26, Get on track a whole Angotia sdk
 - AG-31, Fix error handling when `Engine API` is turned off
+- AG-32, Extend user config character section
--- src/config/user.config.ts
+++ src/config/user.config.ts
@@ -1,6 +1,20 @@
+import { Gender } from "../models/gender.model";
+
 export const userConfig = {
   characters: {
+    maxLevel: 99,
     max: 4,
-    defaultSprite: "https://i.imgur.com/dypMVUP.png"
+    sprite: {
+      default: {
+        [Gender.female]: "https://i.imgur.com/dypMVUP.png",
+        [Gender.male]: "https://i.imgur.com/dypMVUP.png"
+      },
+      size: {
+        x: 144,
+        y: 288
+      },
+      frames: 3,
+      movementDirectionAnimations: 4
+    }
   }
 };
